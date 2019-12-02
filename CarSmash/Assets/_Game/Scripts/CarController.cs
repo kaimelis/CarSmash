2e331344867d2a4587061d167712fc04248217a1
Kotryna <kaimelisvy24@outlook.com>
2019-12-02 12:40:47 +0200

    First commit

---
+++ CarSmash/Assets/_Game/Scripts/CarController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float _speed = 5.0f;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Driving forward");
+        }
+    }
+
+}
